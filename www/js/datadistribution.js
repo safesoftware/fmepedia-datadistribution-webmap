f0f982e192d5eb6a48f11f5e9e27f0b6dbe297ee
amundhak <amundhak@gmail.com>
2014-05-14 15:38:31 -0700

    Fixed URL trailing bug.

--- www/js/datadistribution.js
+++ www/js/datadistribution.js
@@ -56,7 +56,7 @@ var dataDist = (function () {
    */
   function buildURL(formInfo){
     var str = '';
-    str = host + '/fmedatadownload/' + repository + '/' + workspaceName + '.fmw?';
+    str = host + '/fmedatadownload/' + repository + '/' + workspaceName + '?';
     var elem = formInfo[0];
     for(var i = 0; i < elem.length; i++) {
       if(elem[i].type !== 'submit') {
