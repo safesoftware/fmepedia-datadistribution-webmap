b7a92717a35277f36a808dda237394b8b05fcc39
Laura Wu <laura.wu@safe.com>
2020-06-09 15:31:24 -0700

    Split code into separate js files

--- www/js/arcgis/arcgis-polygoncontrol.js
+++ www/js/arcgis/arcgis-polygoncontrol.js
@@ -1,20 +1,54 @@
+var myMap;
+
 function ArcGISPolygonDrawTools(myArcGISMap){
-	this.arcgisMap = myArcGISMap;
+	myMap = myArcGISMap;
 }
 
 /**
  * Fired when the user clicks the draw polygon button
  */
 ArcGISPolygonDrawTools.prototype.drawPolygon = function() {
-	this.clearPolygon();
-	this.arcgisMap.toolbar.activate(esri.toolbars.Draw.POLYGON);
+	//clearing ay graphics and starting a new drawing
+	view.graphics.removeAll();
+
+	//starts creating a polygon graphic on first click
+	var action = draw.create("polygon", {mode: "click"});
+
+	// PolygonDrawAction.vertex-add
+	// Fires when user clicks, or presses the "F" key.
+	// Can also be triggered when the "R" key is pressed to redo.
+	action.on("vertex-add", function (evt) {
+		myMap.createPolygonGraphic(evt.vertices);
+	});
+
+	// PolygonDrawAction.vertex-remove
+	// Fires when the "Z" key is pressed to undo the last added vertex
+	action.on("vertex-remove", function (evt) {
+		myMap.createPolygonGraphic(evt.vertices);
+	});
+
+	// Fires when the pointer moves over the view
+	action.on("cursor-update", function (evt) {
+		myMap.createPolygonGraphic(evt.vertices);
+	});
+
+	// Add a graphic representing the completed polygon
+	// when user double-clicks on the view or presses the "C" key
+	action.on("draw-complete", function (evt) {
+		myMap.createPolygonGraphic(evt.vertices);
+
+		//once the polygon is complete get the coordinates for vetices
+		//and construct WKT polygon
+		var polygon = myMap.createPolygonGraphic(evt.vertices);
+		document.getElementById('geom').value = myMap.getPolygonCoordsText(polygon);
+	});
 }
 /**
  * Called when the user clicks the Clear button
  */
 ArcGISPolygonDrawTools.prototype.clearPolygon = function() {
 
-	this.arcgisMap.toolbar.deactivate(esri.toolbars.Draw.POLYGON);
-	this.arcgisMap.arcgisMap.graphics.clear();
+	view.graphics.removeAll();
 	document.getElementById('geom').value = "";
+
 }
